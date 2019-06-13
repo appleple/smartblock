d8efcac35bdc0671a5de0fe432c46bb661764e9e
Hori Godai <ess_president@me.com>
2019-06-13 12:33:36 +0900

    use hooks for paper-editor

--- src/components/paper-editor.tsx
+++ src/components/paper-editor.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 import { EditorView } from 'prosemirror-view'
 import { Schema, Node, DOMParser, DOMSerializer } from 'prosemirror-model'
 import { keymap } from 'prosemirror-keymap'
-
 import { chainCommands } from 'prosemirror-commands'
 import scrollTo from 'scroll-to'
 import { EditorState } from 'prosemirror-state'
@@ -19,8 +18,10 @@ import CustomLayout from './custom-layout'
 import plugins from '../config/plugins'
 import keys from '../config/keys'
 import { getScrollTop, getOffset, getViewport } from '../utils'
-import extensions from '../extensions'
+import defaultExtensions from '../extensions'
 import { Extension } from '../types'
+
+const { useState, useRef } = React;
 
 const Input = styled('div')`
   width: 100%;
@@ -53,321 +54,297 @@ type AppProps = {
   showBackBtn?: boolean
 }
 
-type AppState = {
-  doc: Node
-  containerId: string,
-  showMenus: boolean
-}
-
 const EDITMENUHEIGHT = 80;
 
-export default class App extends React.Component<AppProps, AppState> {
-  container: HTMLElement
-  view?: EditorView;
+const getBlockSchemas = (extensions: Extension[]) => {
+  const nodesSchema = extensions.filter(extension => {
+    if (extension.schema && extension.schema.group === 'block') {
+      return true
+    }
+    return false
+  })
+  const nodes = nodesSchema.reduce((node, curr, index) => {
+    const newNode = { [curr.name]: { ...curr.schema } }
+    return { ...node, ...newNode }
+  }, {})
+  return nodes
+}
 
-  schema!: Schema
+const getBlocks = (extensions: Extension[]) => {
+  const nodesSchema = extensions.filter(extension => {
+    if (extension.group === 'block') {
+      return true
+    }
+    return false
+  })
+  return nodesSchema
+}
 
-  static defaultProps = {
-    extensions,
+const getMarks = (extensions: Extension[]) => {
+  const marksSchema = extensions.filter(extension => {
+    if (extension.group === 'mark') {
+      return true
+    }
+    return false
+  })
+  return marksSchema
+}
+
+const getMarkSchemas = (extensions: Extension[]) => {
+  const marksSchema = getMarks(extensions)
+  const marks = marksSchema.reduce((mark, curr, index) => {
+    const newMark = { [curr.name]: { ...curr.schema } }
+    return { ...mark, ...newMark }
+  }, {})
+  return marks
+}
+
+const getEdits = (extensions: Extension[]) => {
+  const editMarks = extensions.filter(extension => {
+    if (extension.group === 'edit') {
+      return true
+    }
+    return false
+  })
+  return editMarks
+}
+
+const getSchemaBlockDependencies = (extensions: Extension[]) => {
+  const schemas = extensions.reduce((schema, curr) => {
+    if (curr.schemaDependencies) {
+      return Object.assign({}, schema, curr.schemaDependencies)
+    }
+    return schema
+  }, {})
+  return schemas
+}
+
+const getSchemaFromExtensions = (extensions: Extension[]) => {
+  let nodes = getBlockSchemas(extensions)
+  const nodeDependencies = getSchemaBlockDependencies(extensions)
+  const base = {
+    doc: {
+      content: 'block+'
+    },
+    text: {
+      group: 'inline'
+    },
+    hard_break: {
+      inline: true,
+      group: 'inline',
+      selectable: false,
+      parseDOM: [{ tag: 'br' }],
+      toDOM() {
+        return ['br']
+      }
+    }
+  }
+  nodes = { ...nodes, ...base, ...nodeDependencies }
+  const marks = getMarkSchemas(extensions)
+  return new Schema({ nodes, marks } as { nodes: any; marks: any })
+}
+
+const getHtmlFromNode = (doc: Node, schema: Schema) => {
+  const fragment = DOMSerializer.fromSchema(schema).serializeFragment(
+    doc.content
+  )
+  const div = document.createElement('div')
+  div.appendChild(fragment)
+  return div.innerHTML
+}
+
+const getKeys = (extensions: Extension[], schema: Schema) => {
+  const extensionKeys = {}
+  extensions.forEach(extension => {
+    if (extension.keys) {
+      const registeredKeys = extension.keys(schema)
+      Object.keys(registeredKeys).forEach(key => {
+        if (!extensionKeys[key]) {
+          extensionKeys[key] = []
+        }
+        extensionKeys[key].push(registeredKeys[key])
+      })
+    }
+  })
+
+  Object.keys(keys).forEach(key => {
+    if (!extensionKeys[key]) {
+      extensionKeys[key] = []
+    }
+    extensionKeys[key].push(keys[key])
+  })
+
+  const keyMaps = {}
+
+  Object.keys(extensionKeys).forEach(extensionKey => {
+    keyMaps[extensionKey] = chainCommands(...extensionKeys[extensionKey])
+  })
+
+  return keymap(keyMaps)
+}
+
+const getMenu = (extensions: Extension[]) => {
+  return extensions.filter(extension => extension.showMenu)
+}
+
+const onChange = (
+  state: EditorState,
+  dispatch: typeof EditorView.prototype.dispatch,
+  props: AppProps,
+  schema: Schema,
+  container?: React.MutableRefObject<HTMLDivElement>
+) => {
+  const { doc } = state
+  if (container && container.current) {
+    const selected = container.current.querySelector(
+      '.selected'
+    ) as HTMLDivElement
+    if (selected) {
+      const viewport = getViewport()
+      const top = getScrollTop() + viewport.height
+      const offsetTop = getOffset(selected).top
+      if (offsetTop + EDITMENUHEIGHT >= top) {
+        if (
+          /iPod|iPhone|iPad/.test(navigator.platform) &&
+          document.activeElement
+        ) {
+          const activeElement = document.activeElement as HTMLElement
+          if (activeElement.isContentEditable) {
+            scrollTo(0, offsetTop - EDITMENUHEIGHT)
+          }
+        } else {
+          scrollTo(0, offsetTop - EDITMENUHEIGHT)
+        }
+      }
+    }
+  }
+  if (props.onChange) {
+    const html = getHtmlFromNode(doc, schema)
+    props.onChange({
+      json: doc.content,
+      html,
+      schema
+    })
+  }
+  const { childCount } = doc.content
+  const lastNode = doc.content.child(childCount - 1)
+  if (lastNode.type.name !== 'paragraph') {
+    const { paragraph } = state.schema.nodes
+    dispatch(
+      state.tr.insert(state.doc.content.size, paragraph.createAndFill())
+    )
+  }
+}
+
+const getPlugins = (extensions: Extension[], schema: Schema) => {
+  let customPlugins = []
+  extensions.forEach(extension => {
+    if (extension.plugins) {
+      customPlugins = [...customPlugins, ...extension.plugins]
+    }
+  })
+  const keyPlugin = getKeys(extensions, schema)
+  return [...plugins, ...customPlugins, keyPlugin]
+}
+
+const getNodeViews = (extensions: Extension[]) => {
+  const views = {}
+  extensions.forEach(extension => {
+    if (extension.render) {
+      views[extension.name] = (node: Node, view: EditorView, getPos) => {
+        const dom = document.createElement('div')
+        requestAnimationFrame(() => {
+          ReactDOM.render(<>{extension.render(node, view, getPos)}</>, dom)
+        })
+
+        return {
+          dom,
+          ignoreMutation() {
+            return true
+          },
+          stopEvent() {
+            return true
+          }
+        }
+      }
+    }
+  })
+  return views
+}
+
+export default (props: AppProps) => {
+  const defaultProps = {
+    extensions: defaultExtensions,
     offsetTop: 0,
     showBackBtn: false
   }
 
-  constructor(props) {
-    super(props)
-    const { html, json, extensions } = props
-    const schema = this.getSchemaFromExtensions(extensions)
-    this.schema = schema
-    let realHtml = html
+  props = Object.assign({}, defaultProps, props);
+  const { html, json, extensions, showBackBtn } = props
+  const schema = getSchemaFromExtensions(props.extensions)
+  let realHtml = html
 
-    if (json) {
-      const node = Node.fromJSON(this.schema, json)
-      realHtml = this.getHtmlFromNode(node, schema)
-    }
-    const div = document.createElement('div')
-    div.innerHTML = realHtml
-    const doc = DOMParser.fromSchema(this.schema).parse(div)
-    if (this.props.onInit) {
-      this.props.onInit({
-        schema
-      })
-    }
-    this.state = { doc, containerId: uuid(), showMenus: true }
+  if (json) {
+    const node = Node.fromJSON(schema, json)
+    realHtml = getHtmlFromNode(node, schema)
   }
-
-  getBlockSchemas(extensions: Extension[]) {
-    const nodesSchema = extensions.filter(extension => {
-      if (extension.schema && extension.schema.group === 'block') {
-        return true
-      }
-      return false
+  const div = document.createElement('div')
+  div.innerHTML = realHtml
+  const doc = DOMParser.fromSchema(schema).parse(div)
+  if (props.onInit) {
+    props.onInit({
+      schema
     })
-    const nodes = nodesSchema.reduce((node, curr, index) => {
-      const newNode = { [curr.name]: { ...curr.schema } }
-      return { ...node, ...newNode }
-    }, {})
-    return nodes
   }
 
-  getBlocks(extensions: Extension[]) {
-    const nodesSchema = extensions.filter(extension => {
-      if (extension.group === 'block') {
-        return true
-      }
-      return false
-    })
-    return nodesSchema
-  }
+  const [showMenus, setShowMenus] = useState(true);
+  const containerId = React.useMemo(() => {
+    return uuid();
+  }, []);
 
-  getMarkSchemas(extensions: Extension[]) {
-    const marksSchema = this.getMarks(extensions)
-    const marks = marksSchema.reduce((mark, curr, index) => {
-      const newMark = { [curr.name]: { ...curr.schema } }
-      return { ...mark, ...newMark }
-    }, {})
-    return marks
-  }
+  const container = useRef<HTMLDivElement>(null);
+  const editorOptions = { schema, plugins: getPlugins(extensions, schema), doc }
+  const blocks = getBlocks(extensions)
+  const marks = getMarks(extensions)
+  const edits = getEdits(extensions)
+  const nodeViews = getNodeViews(extensions)
 
-  getMarks(extensions: Extension[]) {
-    const marksSchema = extensions.filter(extension => {
-      if (extension.group === 'mark') {
-        return true
-      }
-      return false
-    })
-    return marksSchema
-  }
-
-  getEdits(extensions: Extension[]) {
-    const editMarks = extensions.filter(extension => {
-      if (extension.group === 'edit') {
-        return true
-      }
-      return false
-    })
-    return editMarks
-  }
-
-  getSchemaBlockDependencies(extensions: Extension[]) {
-    const schemas = extensions.reduce((schema, curr) => {
-      if (curr.schemaDependencies) {
-        return Object.assign({}, schema, curr.schemaDependencies)
-      }
-      return schema
-    }, {})
-    return schemas
-  }
-
-  getSchemaFromExtensions(extensions: Extension[]) {
-    let nodes = this.getBlockSchemas(extensions)
-    const nodeDependencies = this.getSchemaBlockDependencies(extensions)
-    const base = {
-      doc: {
-        content: 'block+'
-      },
-      text: {
-        group: 'inline'
-      },
-      hard_break: {
-        inline: true,
-        group: 'inline',
-        selectable: false,
-        parseDOM: [{ tag: 'br' }],
-        toDOM() {
-          return ['br']
-        }
-      }
-    }
-    nodes = { ...nodes, ...base, ...nodeDependencies }
-    const marks = this.getMarkSchemas(extensions)
-    return new Schema({ nodes, marks } as { nodes: any; marks: any })
-  }
-
-  getHtmlFromNode(doc: Node, schema: Schema) {
-    const fragment = DOMSerializer.fromSchema(schema).serializeFragment(
-      doc.content
-    )
-    const div = document.createElement('div')
-    div.appendChild(fragment)
-    return div.innerHTML
-  }
-
-  getKeys(extensions: Extension[]) {
-    const extensionKeys = {}
-
-    extensions.forEach(extension => {
-      if (extension.keys) {
-        const registeredKeys = extension.keys(this.schema)
-        Object.keys(registeredKeys).forEach(key => {
-          if (!extensionKeys[key]) {
-            extensionKeys[key] = []
-          }
-          extensionKeys[key].push(registeredKeys[key])
-        })
-      }
-    })
-
-    Object.keys(keys).forEach(key => {
-      if (!extensionKeys[key]) {
-        extensionKeys[key] = []
-      }
-      extensionKeys[key].push(keys[key])
-    })
-
-    const keyMaps = {}
-
-    Object.keys(extensionKeys).forEach(extensionKey => {
-      keyMaps[extensionKey] = chainCommands(...extensionKeys[extensionKey])
-    })
-
-    return keymap(keyMaps)
-  }
-
-  getPlugins() {
-    const { extensions } = this.props
-    let customPlugins = []
-    extensions.forEach(extension => {
-      if (extension.plugins) {
-        customPlugins = [...customPlugins, ...extension.plugins]
-      }
-    })
-    const keyPlugin = this.getKeys(extensions)
-    return [...plugins, ...customPlugins, keyPlugin]
-  }
-
-  getNodeViews() {
-    const { extensions } = this.props
-    const views = {}
-    extensions.forEach(extension => {
-      if (extension.render) {
-        views[extension.name] = (node: Node, view: EditorView, getPos) => {
-          const dom = document.createElement('div')
-          requestAnimationFrame(() => {
-            ReactDOM.render(<>{extension.render(node, view, getPos)}</>, dom)
-          })
-
-          return {
-            dom,
-            ignoreMutation() {
-              return true
-            },
-            stopEvent() {
-              return true
-            }
-          }
-        }
-      }
-    })
-    return views
-  }
-
-  getMenu(extensions: Extension[]) {
-    return extensions.filter(extension => extension.showMenu)
-  }
-
-  onChange = (
-    state: EditorState,
-    dispatch: typeof EditorView.prototype.dispatch
-  ) => {
-    const { doc } = state
-    if (this.container) {
-      const selected = this.container.querySelector(
-        '.selected'
-      ) as HTMLDivElement
-      if (selected) {
-        const viewport = getViewport()
-        const top = getScrollTop() + viewport.height
-        const offsetTop = getOffset(selected).top
-        const height = selected.offsetHeight
-        if (offsetTop + EDITMENUHEIGHT >= top) {
-          if (
-            /iPod|iPhone|iPad/.test(navigator.platform) &&
-            document.activeElement
-          ) {
-            const activeElement = document.activeElement as HTMLElement
-            if (activeElement.isContentEditable) {
-              scrollTo(0, offsetTop - EDITMENUHEIGHT)
-            }
-          } else {
-            scrollTo(0, offsetTop - EDITMENUHEIGHT)
-          }
-        }
-      }
-    }
-    if (this.props.onChange) {
-      const { schema } = this
-      const html = this.getHtmlFromNode(doc, this.schema)
-      this.props.onChange({
-        json: doc.content,
-        html,
-        schema
-      })
-    }
-    const { childCount } = doc.content
-    const lastNode = doc.content.child(childCount - 1)
-    if (lastNode.type.name !== 'paragraph') {
-      const { paragraph } = state.schema.nodes
-      dispatch(
-        state.tr.insert(state.doc.content.size, paragraph.createAndFill())
-      )
-    }
-  }
-
-  clearSelection = (e) => {
-    const { containerId } = this.state;
-    if (e.target.getAttribute('id') === containerId) {
-      this.setState({
-        showMenus: false
-      });
+  return (<div id={containerId} onClick={(e) => {
+    const target = e.target as HTMLDivElement;
+    if (target.getAttribute('id') === containerId) {
+      setShowMenus(false);
     } else {
-      this.setState({
-        showMenus: true
-      });
+      setShowMenus(true);
     }
-  }
-
-  render() {
-    const { extensions, showBackBtn } = this.props
-    const { doc, containerId, showMenus } = this.state
-    const { schema } = this
-    const editorOptions = { schema, plugins: this.getPlugins(), doc }
-    const blocks = this.getBlocks(extensions)
-    const marks = this.getMarks(extensions)
-    const edits = this.getEdits(extensions)
-    const nodeViews = this.getNodeViews()
-
-    return (<div id={containerId} onClick={this.clearSelection}>
-      <Container>
-        <div
-          className={showMenus ? '' : 'ProseMirrorHideSelection'}
-          ref={ref => {
-            if (ref && !this.container) {
-              this.container = ref
-              this.forceUpdate()
+  }}>
+    <Container>
+      <div
+        className={showMenus ? '' : 'ProseMirrorHideSelection'}
+        ref={container}
+      >
+        <Input>
+          <Editor
+            options={editorOptions}
+            nodeViews={nodeViews}
+            onChange={(state, dispatch) => {
+              onChange(state,dispatch, props, schema, container);
+            }}
+            render={({ editor, view }: ProseRender) => (
+              <>
+                {showMenus && <>
+                  <Menu view={view} menu={getMenu(blocks)} />
+                  <EditMenu view={view} menu={getMenu(edits)} />
+                  <InlineMenu menu={getMenu(marks)} view={view} />
+                  <CustomLayout view={view} menu={getMenu(blocks)} />
+                  {showBackBtn && <BackBtn view={view} />}
+                </>}
+                {editor}
+              </>)
             }
-          }}
-        >
-          <Input>
-            <Editor
-              options={editorOptions}
-              nodeViews={nodeViews}
-              onChange={this.onChange}
-              render={({ editor, view }: ProseRender) => (
-                <>
-                  {showMenus && <>
-                    <Menu view={view} menu={this.getMenu(blocks)} />
-                    <EditMenu view={view} menu={this.getMenu(edits)} />
-                    <InlineMenu menu={this.getMenu(marks)} view={view} />
-                    <CustomLayout view={view} menu={this.getMenu(blocks)} />
-                    {showBackBtn && <BackBtn view={view} />}
-                  </>}
-                  {editor}
-                </>)
-              }
-            />
-          </Input>
-        </div>
-      </Container>
-    </div>)
-  }
+          />
+        </Input>
+      </div>
+    </Container>
+  </div>)
+  
 }
