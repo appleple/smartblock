9007ea3ac360e9cc241b48479a9619690722f9d4
Hori Godai <ess_president@me.com>
2019-06-25 15:09:51 +0900

    fix: list-item schema

--- src/extensions/list-item.tsx
+++ src/extensions/list-item.tsx
@@ -24,7 +24,7 @@ export default class ListItem extends Extension {
       return this.customSchema;
     }
     return {
-      content: 'paragraph block*',
+      content: 'paragraph',
       group: 'block',
       parseDOM: [
         {
