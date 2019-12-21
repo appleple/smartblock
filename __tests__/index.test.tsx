13c7fec2ee6fdeccae9c29fd8a5d855421ce33d8
steelydylan <ess_president@me.com>
2019-12-21 23:43:38 +0900

    test: add utils test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,9 +2,22 @@ import * as React from 'react';
 import * as TestRenderer from 'react-test-renderer'
 import Paragraph from '../src/extensions/paragraph';
 import { setTextSelection } from 'prosemirror-utils';
-import { TextSelection } from 'prosemirror-state';
 import EditMenu from '../src/components/edit-menu';
 import { getEditorViewFromExtensions } from './util';
+import { getParentNodeFromState } from '../src/utils';
+
+describe('utils test', () => {
+  it('should get proper node from pos', () => {
+    const view = getEditorViewFromExtensions([new Paragraph()]);
+    view.dispatch(
+      setTextSelection(1)(
+        view.state.tr
+      )
+    );
+    const node = getParentNodeFromState(view.state);
+    expect(node.type.name).toEqual('paragraph');
+  });
+});
 
 describe('edit-menu', () => {
   it('should exist', () => {
@@ -27,14 +40,24 @@ describe('edit-menu', () => {
     });
     // not focused
     expect(item.props.style.top).toBe(-1000);
-    // TestRenderer.act(() => {
-    //   view.dispatch(
-    //     setTextSelection(0)(
-    //       view.state.tr
-    //     )
-    //   );
-    //   console.log(view);
-    //   editMenu.update(<EditMenu view={view} menu={[]} />);
-    // });
+    view.dispatch(
+      setTextSelection(1)(
+        view.state.tr
+      )
+    );
+
+    /* mock view.coordsAtPos */
+    view.coordsAtPos = (pos) => ({
+      top: 100,
+      left: 0,
+      right: 0,
+      bottom: 0
+    });
+
+    TestRenderer.act(() => {
+      editMenu.update(<EditMenu view={view} menu={[]} />);
+    });
+    expect(item.props.style.top).not.toBe(0);
+    expect(item.props.style.top).not.toBe(-1000);
   });
 })
