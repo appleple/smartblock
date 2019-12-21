a64e12f97a2173be83785ebd03b5876eddeb77c0
steelydylan <ess_president@me.com>
2019-12-21 13:18:40 +0900

    fix

--- lib/components/button.d.ts
+++ lib/components/button.d.ts
@@ -1,5 +1,4 @@
-import * as React from 'react';
-declare const Button: import("styled-components").StyledComponent<({ tag, children, ...props }: any) => React.DetailedReactHTMLElement<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, any, {
+declare const Button: import("styled-components").StyledComponent<"button", any, {
     active?: boolean;
     color?: "black" | "white";
 }, never>;
