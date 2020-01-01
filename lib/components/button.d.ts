30147495347ad5e85b7fdf09c06e8dddf3af7f8f
steelydylan <ess_president@me.com>
2020-01-01 22:31:27 +0900

    chore: fix button

--- lib/components/button.d.ts
+++ lib/components/button.d.ts
@@ -1,7 +1,5 @@
-import * as React from 'react';
-declare type ButtonProps = {
+declare const Button: import("styled-components").StyledComponent<"button", any, {
     active?: boolean;
-    color?: 'black' | 'white';
-};
-declare const _default: import("styled-components").StyledComponent<({ tag, children, ...props }: any) => React.DetailedReactHTMLElement<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, any, ButtonProps, never>;
-export default _default;
+    color?: "black" | "white";
+}, never>;
+export default Button;
