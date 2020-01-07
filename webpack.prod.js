77d1f9e74ec55dc7d5d4bffce52043670a52b500
Hori Godai <ess_president@me.com>
2020-01-07 19:50:03 +0900

    test: build with preact

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,12 +9,9 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     alias: {
-      'react': 'preact-compat',
-      'react-dom': 'preact-compat',
-      // Not necessary unless you consume a module using `createClass`
-      'create-react-class': 'preact-compat/lib/create-react-class',
-      // Not necessary unless you consume a module requiring `react-dom-factories`
-      'react-dom-factories': 'preact-compat/lib/react-dom-factories'
+      'react': 'preact/compat',
+      "react-dom/test-utils": "preact/test-utils",
+      'react-dom': 'preact/compat',
     }
   },
   module: {
