1f1b1edcd9b9faed5ba500da911ad74d50ed992b
Hori Godai <ess_president@me.com>
2020-01-07 19:41:58 +0900

    test: preact

---
+++ src/adapter/index.tsx
@@ -0,0 +1,74 @@
+import { render } from 'preact';
+
+export {
+  /* components */
+  SmartBlock,
+  Button,
+  EditMenu,
+  Editor,
+  InlineMenu,
+  Menu,
+  GlobalStyle,
+  /* 
+    extensions 
+  */
+  /* blocks */
+  Extensions,
+  Link,
+  Image,
+  BulletList,
+  CustomBlock,
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+  ListItem,
+  MoveDown,
+  MoveUp,
+  OrderedList,
+  Paragraph,
+  Blockquote,
+  Table,
+  Embed,
+  Code,
+  /* inline */
+  Emphasis,
+  Strike,
+  Strong,
+  Underline,
+  CustomMark,
+  Trash,
+  /* utils */
+  getViewport,
+  isInput,
+  markActive,
+  blockActive,
+  canInsert,
+  findNodePosition,
+  findSelectedNodeWithType,
+  getParentNodePosFromState,
+  createTable,
+  liftListItem,
+  /* types */
+  Dispatch,
+  Extension,
+  /* defaults */
+  DefaultKeys,
+  DefaultPlugins,
+  /* official */
+  Plugin,
+  /* icons */
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  Heading4Icon,
+  Heading5Icon,
+  Heading6Icon
+} from '../';
+
+
+export default () => {
+
+}
