36e81cbdbd5ddd1c7f703668c54c08323c48e9c2
Hori Godai <ess_president@me.com>
2020-01-09 11:16:06 +0900

    chore(webpack): adjust output

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,45 @@
 const path = require('path');
 const { CheckerPlugin } = require('awesome-typescript-loader');
 
+// for not jsx users
 module.exports = {
   mode: "production",
   entry: {
-    main: './src/adapter/index.tsx'
+    SmartBlock: './src/adapter/index.tsx',
+    Code: './src/extensions/code/index.tsx',
+    Embed: './src/extensions/embed/index.tsx',
+    Extensions: './src/extensions/base.tsx',
+    Image: './src/extensions/image/index.tsx',
+    Link: './src/extensions/link/index.tsx',
+    Table: './src/extensions/table/index.tsx',
+    Blockquote: './src/extensions/blockquote.tsx',
+    BulletList: './src/extensions/bullet-list.tsx',
+    CustomBlock: './src/extensions/custom-block.tsx',
+    CustomMark: './src/extensions/custom-mark.tsx',
+    DefaultKeys: './src/extensions/default-keys.tsx',
+    DefaultPlugins: './src/extensions/default-plugins.tsx',
+    Emphasis: './src/extensions/emphasis.tsx',
+    Heading1: './src/extensions/heading1.tsx',
+    Heading2: './src/extensions/heading2.tsx',
+    Heading3: './src/extensions/heading3.tsx',
+    Heading4: './src/extensions/heading4.tsx',
+    Heading5: './src/extensions/heading5.tsx',
+    Heading6: './src/extensions/heading6.tsx',
+    ListItem: './src/extensions/list-item.tsx',
+    MoveDown: './src/extensions/move-down.tsx',
+    MoveUp: './src/extensions/move-up.tsx',
+    OrderedList: './src/extensions/ordered-list.tsx',
+    Paragraph: './src/extensions/paragraph.tsx',
+    Strike: './src/extensions/strike.tsx',
+    Strong: './src/extensions/strong.tsx',
+    Trash: './src/extensions/trash.tsx',
+    Underline: './src/extensions/underline.tsx'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: "bundle",
-    filename: '[name].js',
-    library: 'SmartBlock',
+    filename: (data) => `${data.chunk.name.toLowerCase()}.js`,
+    library: '[name]',
     libraryExport: "default",
     libraryTarget: 'umd'
   },
