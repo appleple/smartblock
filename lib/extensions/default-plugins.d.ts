f9ed979faafca3375dce0f0490af56a34c0f6abb
Hori Godai <ess_president@me.com>
2020-01-09 12:47:24 +0900

    chore: build lib

--- lib/extensions/default-plugins.d.ts
+++ lib/extensions/default-plugins.d.ts
@@ -1,8 +1,6 @@
 import { Extension } from '../types';
 import 'prosemirror-tables/style/tables.css';
 import 'prosemirror-gapcursor/style/gapcursor.css';
-import '@aeaton/prosemirror-footnotes/style/footnotes.css';
-import '@aeaton/prosemirror-placeholder/style/placeholder.css';
 declare type Config = {
     placeholder: string;
 };
