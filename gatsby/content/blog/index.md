13d27d56326837abb9a6c58c6047739fa739eaf7
Hori Godai <ess_president@me.com>
2020-01-15 19:14:24 +0900

    fix: index page

---
+++ gatsby/content/blog/index.md
@@ -0,0 +1,106 @@
+---
+title: Hello World
+date: "2015-05-01T22:12:03.284Z"
+description: "Hello World"
+---
+
+
+## Features
+
+*   Easy to use with SmartPhone
+*   Fully customizable
+*   Block based
+*   Keep clean HTML and wipe out unnecessary tags
+*   Get the result as **HTML** or **JSON**
+*   copy and paste contents
+
+## Install
+
+```sh
+$ npm install smartblock --save
+```
+
+## Usage
+
+```js
+import * as React from 'react';
+import { render } from 'react-dom';
+import { SmartBlock, GlobalStyle, Extensions } from 'smartblock';
+
+render(<>
+  <GlobalStyle />
+  <SmartBlock 
+    extensions={Extensions}
+    html={'<h2>Hello World</h2><p>hello</p>'}
+    onChange={({ json, html }) => { console.log(json, html);}}  
+  />
+</>, document.getElementById("app"));
+```
+
+## Extensions
+
+### Blocks
+
+- Pragraph
+- Heading1
+- Heading2
+- Heading3
+- Heading4
+- Heading5
+- Heading6
+- OrderdList
+- BulletList
+- ListItem
+- Blockquoe
+- Table
+- Code
+- Image
+
+### Marks
+
+- Emphasis
+- Strike
+- Strong
+- Underline
+
+### Utils
+
+- MoveDown
+- MoveUp
+- Trash
+- DefaultKeys
+- DefaultPlugins
+
+## Customize
+
+You can add custom block like this
+
+```js
+import { Extensions, CustomBlock, CustomMark } from 'smartblock';
+Extensions.push(new CustomBlock({  tagName: 'div',  className: '.alert',  icon: <SomeIconComponent />});
+
+render(<>
+  <GlobalStyle />
+  <SmartBlock 
+    extensions={Extensions}
+    html={'<h2>Hello World</h2><p>hello</p>'}
+    onChange={({ json, html }) => { console.log(json, html);}}  
+  />
+</>, document.getElementById("app"));
+```
+
+You can also add custom inline element like this
+
+```js
+import { Extension, CustomBlock, CustomMark } from 'smartblock';
+Extension.push(new CustomMark({  tagName: 'span',  className: '.small',  icon: <SomeIconComponent />});
+
+render(<>
+  <GlobalStyle />
+  <SmartBlock 
+    extensions={Extensions}
+    html={'<h2>Hello World</h2><p><small>hello</small></p>'}
+    onChange={({ json, html }) => { console.log(json, html);}}  
+  />
+</>, document.getElementById("app"));
+```
--- gatsby/src/components/top.js
+++ gatsby/src/components/top.js
@@ -4,21 +4,11 @@ const SmartBlock = React.lazy(() => import('smartblock/lib/components/smartblock
 const GlobalStyle = React.lazy(() => import('smartblock/lib/utils/style'));
 
 
-const html = `<p>SmartBlock is a block styled editor created by JavaScript. Which gives you nice experience of editing contents at touchscreen. On this page you can see it in action. Try to edit this text ! :)</p>
-<h2>Features</h2>
-<ul>
-  <li>Easy to use with touchscreen device (e.g. smartphone, tablet)</li>  
-  <li>Fully customizable</li>
-  <li>Block styled editor</li>
-  <li>Keep clean HTML and wipe out unnecessary tags</li>
-  <li>Get the result as HTML or JSON</li>
-  <li>Keep style when copy and paste contents</li>
-</ul>
-<p style="text-align:center;">SmartBlock is provided with MIT license. <br/>Made by : @appleple ♥ OSS</p>`;
-
-export default () => {
+export default (props) => {
 
   const isSSR = typeof window === "undefined"
+  const { data } = props;
+  const posts = data.allMarkdownRemark.edges
   
   return (<>
     <section className="hero is-center firstview">
@@ -51,40 +41,14 @@ export default () => {
               </React.Suspense>
             )}
             </div>
-            <h2 style={{ marginTop: '0'}}>Installation</h2>
-            <h3>npm package</h3>
-            <p>Install the package via npm</p>
-            <pre><code>$ npm install smartblock --save</code></pre>
-            <h3>Load script from CDN</h3>
-            <p>Download smartblock.js file to your project and load it.</p>
-            <pre><code className="html">&lt;script src="https://unpkg.com/smartblock@1.2.1/dist/smartblock.js"&gt;&lt;/script&gt;</code></pre>
-            <h2>Usage</h2>
-            <h3>As an React</h3>
-            {/* <pre><code className="jsx">import * as React from 'react';
-import {render} from 'react-dom';
-import {SmartBlock, GlobalStyle} from 'smartblock';
-              
-              render(&lt;&gt;
-                &lt;GlobalStyle /&gt;
-                &lt;SmartBlock
-                  html="&lt;h2&gt;Hello World&lt;/h2&gt;&lt;p&gt;Hello&lt;/p&gt;"
-    onChange={(json, html) =& gt; console.log(html)}
-            /&gt;
-&lt;/&gt;, documeng.getElementById('#app'));</code></pre>
-            <h3>As an native JavaScript</h3>
-            <pre><code className="js">import SmartBlock from 'smartblock/lib/adapter';
-  import Extensions from 'smartblock/lib/extensions';
-  
-SmartBlock('#editor', {
-                extensions: Extensions,
-  onChange: ({html}) => console.log(html)
-});</code></pre> */}
-            <h2>Compatibility</h2>
-            <p>IE11, and Edge, iOS Safari, Firefox, Chrome, Safari</p>
+            {posts.map(({ node }) => {
+              return(<div dangerouslySetInnerHTML={{
+                __html: node.frontmatter.description || node.excerpt,
+              }}>{node.html}</div>);
+            })}
           </div>
         </section>
         </div>
-        
       </main>
   </>);
 }
