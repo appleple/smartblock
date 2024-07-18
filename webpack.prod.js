ffbb4deef0fffde15145b1945872a0cc76bcd377
宇井 陸登 <ui@appleple.com>
2024-07-18 11:11:50 +0900

    fix: standalone で動作しなくなっている問題の修正

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,16 +5,53 @@ const path = require('path');
 const standaloneConfig = merge(common, {
   mode: 'production',
   entry: {
-    'index': './src/adapter/index.tsx',
+    'Editor': './src/adapter/index.tsx',
+    'Extensions': './src/extensions/index.tsx',
+    'Base': './src/extensions/base.tsx',
+    'Code': './src/extensions/code/index.tsx',
+    'Cmbed': './src/extensions/embed/index.tsx',
+    'Image': './src/extensions/image/index.tsx',
+    'Link': './src/extensions/link/index.tsx',
+    'Table': './src/extensions/table/index.tsx',
+    'Blockquote': './src/extensions/blockquote.tsx',
+    'BulletList': './src/extensions/bullet-list.tsx',
+    'CustomBlock': './src/extensions/custom-block.tsx',
+    'CustomMark': './src/extensions/custom-mark.tsx',
+    'DefaultKeys': './src/extensions/default-keys.tsx',
+    'DefaultPlugins': './src/extensions/default-plugins.tsx',
+    'Emphasis': './src/extensions/emphasis.tsx',
+    'Heading1': './src/extensions/heading1.tsx',
+    'Heading2': './src/extensions/heading2.tsx',
+    'Heading3': './src/extensions/heading3.tsx',
+    'Heading4': './src/extensions/heading4.tsx',
+    'Heading5': './src/extensions/heading5.tsx',
+    'Heading6': './src/extensions/heading6.tsx',
+    'ListItem': './src/extensions/list-item.tsx',
+    'MoveDown': './src/extensions/move-down.tsx',
+    'MoveUp': './src/extensions/move-up.tsx',
+    'OrderedList': './src/extensions/ordered-list.tsx',
+    'Paragraph': './src/extensions/paragraph.tsx',
+    'Strike': './src/extensions/strike.tsx',
+    'Strong': './src/extensions/strong.tsx',
+    'Trash': './src/extensions/trash.tsx',
+    'Underline': './src/extensions/underline.tsx',
   },
   output: {
     library: {
-      name: ['SmartBlock', 'Editor'],
+      name: ['SmartBlock', '[name]'],
       type: 'umd',
       export: 'default',
     },
-    path: path.resolve(__dirname, 'dist/umd/standalone'),
-    filename: (data) => `${data.chunk.name.toLowerCase()}.js`,
+    path: path.resolve(__dirname, 'dist/standalone'),
+    filename: (data) => {
+      if (data.chunk.name === 'Editor') {
+        return 'index.js'
+      }
+      if (data.chunk.name === 'Extensions') {
+        return 'extensions/index.js';
+      }
+      return `extensions/${data.chunk.name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()}.js`;
+    },
   },
 });
 
@@ -62,6 +99,9 @@ const defaultExportConfig = merge(common, {
     filename: (data) => {
       if (data.chunk.name === 'SmartBlock') {
         return 'components/smartblock.js';
+      }
+      if (data.chunk.name === 'Extensions') {
+        return 'extensions/index.js';
       }
       return `extensions/${data.chunk.name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()}.js`;
     },
